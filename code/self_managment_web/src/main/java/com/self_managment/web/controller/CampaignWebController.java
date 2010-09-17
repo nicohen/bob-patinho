a9d2bf85a08786bc307625fd9158c9aacb928cc6
duilio.hernandez <duilio.hernandez@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-09-17 02:52:48 +0000

    campaign crud

---
+++ code/self_managment_web/src/main/java/com/self_managment/web/controller/CampaignWebController.java
@@ -0,0 +1,117 @@
+package com.self_managment.web.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.jsf.FacesContextUtils;
+
+import com.self_managment.model.entity.Agent;
+import com.self_managment.model.entity.Campaign;
+import com.self_managment.service.CRUDService;
+import com.self_managment.web.util.JSFUtil;
+
+@Component("campaignBean")
+@Scope("session")
+public class CampaignWebController {
+    private List<Campaign> campaigns;
+    private Campaign campaign = new Campaign();
+    private CRUDService<Campaign, Serializable> service;
+    private Agent agent;
+
+    @SuppressWarnings("unchecked")
+    public CampaignWebController() {
+	ApplicationContext ctx = FacesContextUtils
+		.getWebApplicationContext(FacesContext.getCurrentInstance());
+
+	service = (CRUDService<Campaign, Serializable>) ctx
+		.getBean("campaignService");
+    }
+
+    public List<Campaign> getCampaigns() {
+	if (campaigns == null)
+	    campaigns = service.findAll();
+	return campaigns;
+    }
+
+    public Campaign getCampaign() {
+	return campaign;
+    }
+
+    public void setCampaign(Campaign campaign) {
+	this.campaign = campaign;
+    }
+
+    public Agent getAgent() {
+	return agent;
+    }
+
+    public void setAgent(Agent agent) {
+	this.agent = agent;
+    }
+
+    public String update() {
+	try {
+	    if (campaign.getId() == null)
+		service.save(campaign);
+	    else
+		service.update(campaign);
+	    setCampaign(new Campaign());
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    JSFUtil.addErrorMessage("Error");
+	} finally {
+	    campaigns = service.findAll();
+	}
+	return "";
+    }
+
+    public String refreshCampaign() {
+	campaign = service.findById(campaign.getId());
+	return "";
+    }
+
+    public String create() {
+	setCampaign(new Campaign());
+	return "";
+    }
+
+    public String createAgent() {
+	setAgent(new Agent());
+	return "";
+    }
+
+    public String addAgent() {
+	if (campaign.getAgents() == null) {
+	    List<Agent> agents = new ArrayList<Agent>();
+	    agents.add(agent);
+	    campaign.setAgents(agents);
+	} else {
+	    campaign.getAgents().add(agent);
+	}
+	return "";
+    }
+
+    public String deleteAgent() {
+	campaign.getAgents().remove(agent);
+	return "";
+    }
+
+    public String delete() {
+	try {
+	    service.delete(campaign);
+	    campaigns.remove(campaign);
+	} catch (Exception e) {
+	    JSFUtil.addErrorMessage("Error");
+	    e.printStackTrace();
+	}/*
+	 * finally { campaigns = service.findAll(); }
+	 */
+	return "";
+    }
+}
--- code/self_managment_web/src/main/webapp/index.jsp
+++ code/self_managment_web/src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
 <%@ page session="false"%>
 <%
-response.sendRedirect("stockCRUD.jsf");
+//response.sendRedirect("stockCRUD.jsf");
+response.sendRedirect("campaignCRUD.jsf");
 %>
