bc1c4288491f8b982b55509a37d0848a86a3649c
sergiocolinas <sergiocolinas@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-10-08 19:10:43 +0000

    S01-007: Se listan los supervisores asociados a la campaña...

---
+++ code/self_managment_web/src/main/java/com/self_managment/web/converter/SupervisorConverter.java
@@ -0,0 +1,40 @@
+package com.self_managment.web.converter;
+
+import java.util.List;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.jsf.FacesContextUtils;
+
+import com.self_managment.model.entity.Supervisor;
+import com.self_managment.service.SupervisorService;
+
+public class SupervisorConverter implements Converter {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Object getAsObject(FacesContext facesContext, UIComponent component,
+	    String string) {
+	ApplicationContext ctx = FacesContextUtils
+		.getWebApplicationContext(facesContext);
+
+	SupervisorService supervisorService = (SupervisorService) ctx
+		.getBean("supervisorService");
+
+	List<Supervisor> l = supervisorService.findAllByProperty("name", string);
+
+	return l.isEmpty() ? null : l.get(0);
+
+    }
+
+    @Override
+    public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
+	if (arg2 == null)
+	    return null;
+	return arg2.toString();
+    }
+
+}
