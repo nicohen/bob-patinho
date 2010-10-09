bcb544bf649d59cb063ae00962528ec2bc8ebfb3
duilio.hernandez <duilio.hernandez@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-10-09 00:14:58 +0000

    S01-007: se pone en null la fk de la campaña de los supervisores eliminados.

--- code/self_managment/src/main/java/com/self_managment/model/entity/Campaign.java
+++ code/self_managment/src/main/java/com/self_managment/model/entity/Campaign.java
@@ -2,6 +2,7 @@ package com.self_managment.model.entity;
 
 import java.util.Date;
 import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,215 +18,216 @@ import org.hibernate.annotations.Cascade;
 
 @Entity
 @Table(name = "campaign", catalog = "self_managment", uniqueConstraints = {
-		@UniqueConstraint(columnNames = "CAMPAIGN_ID"),
-		@UniqueConstraint(columnNames = "CAMPAIGN_NAME") })
+	@UniqueConstraint(columnNames = "CAMPAIGN_ID"),
+	@UniqueConstraint(columnNames = "CAMPAIGN_NAME") })
 public class Campaign implements java.io.Serializable {
-	public enum CampaignType {
-		INBOUND, OUTBOUND;
-	}
+    public enum CampaignType {
+	INBOUND, OUTBOUND;
+    }
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	//@GeneratedValue(strategy = IDENTITY)
-	@Column(name = "CAMPAIGN_ID", unique = true, nullable = false)
-	private Integer id;
+    @Id
+    // @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "CAMPAIGN_ID", unique = true, nullable = false)
+    private Integer id;
 
-//	@Column(name = "CAMPAIGN_CODE", unique = true, nullable = false, length = 10)
-//	private String code;
+    // @Column(name = "CAMPAIGN_CODE", unique = true, nullable = false, length =
+    // 10)
+    // private String code;
 
-	@Column(name = "CAMPAIGN_NAME", unique = true, nullable = false, length = 20)
-	private String name;
-	
-	@Column(name = "CAMPAIGN_DESCRIPTION", nullable = true)
-	private String description;
+    @Column(name = "CAMPAIGN_NAME", unique = true, nullable = false, length = 20)
+    private String name;
 
-	@Column(name = "TYPE", nullable = false)
-	@Enumerated(EnumType.STRING)
-	private CampaignType type;
+    @Column(name = "CAMPAIGN_DESCRIPTION", nullable = true)
+    private String description;
 
-	@Column(name = "OPTIM_VALUE", nullable = false)
-	private Double optimValue;
+    @Column(name = "TYPE", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CampaignType type;
 
-	@Column(name = "OBJETIVE_VALUE", nullable = false)
-	private Double objetiveValue;
+    @Column(name = "OPTIM_VALUE", nullable = false)
+    private Double optimValue;
 
-	@Column(name = "MINIMUM_VALUE", nullable = false)
-	private Double minimumValue;
+    @Column(name = "OBJETIVE_VALUE", nullable = false)
+    private Double objetiveValue;
 
-	@Column(name = "UNSATISFACTORY_VALUE", nullable = false)
-	private Double unsatisfactoryValue;
-	
-	@Column(name = "START_DATE", nullable = false)
-	private Date startDate;
-	
-	@Column(name = "END_DATE", nullable = true)
-	private Date endDate;
+    @Column(name = "MINIMUM_VALUE", nullable = false)
+    private Double minimumValue;
 
-	// @ManyToMany(cascade = CascadeType.PERSIST)
-	// @JoinTable(name = "campaign_agent", joinColumns = @JoinColumn(name =
-	// "CAMPAIGN_ID"), inverseJoinColumns = @JoinColumn(name = "AGENT_ID"))
-	@OneToMany(mappedBy = "campaign")
-	private List<Agent> agents;
-	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "campaign", cascade = {CascadeType.ALL })
-	private List<Supervisor> supervisors;
+    @Column(name = "UNSATISFACTORY_VALUE", nullable = false)
+    private Double unsatisfactoryValue;
 
-	// @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "pk.campaign")
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "pk.campaign", cascade = {
-			CascadeType.PERSIST, CascadeType.MERGE })
-	@Cascade( { org.hibernate.annotations.CascadeType.SAVE_UPDATE,
-			org.hibernate.annotations.CascadeType.DELETE_ORPHAN })
-	private List<CampaignMetric> campaignMetric;
+    @Column(name = "START_DATE", nullable = false)
+    private Date startDate;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		final Campaign other = (Campaign) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+    @Column(name = "END_DATE", nullable = true)
+    private Date endDate;
 
-	public List<Agent> getAgents() {
-		return agents;
-	}
-	
-	public List<Supervisor> getSupervisors() {
-		return supervisors;
-	}
+    // @ManyToMany(cascade = CascadeType.PERSIST)
+    // @JoinTable(name = "campaign_agent", joinColumns = @JoinColumn(name =
+    // "CAMPAIGN_ID"), inverseJoinColumns = @JoinColumn(name = "AGENT_ID"))
+    @OneToMany(mappedBy = "campaign")
+    private List<Agent> agents;
 
-	public List<CampaignMetric> getCampaignMetric() {
-		return campaignMetric;
-	}
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "campaign", cascade = { CascadeType.PERSIST })
+    private List<Supervisor> supervisors;
 
-//	public String getCode() {
-//		return code;
-//	}
+    // @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "pk.campaign")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "pk.campaign", cascade = {
+	    CascadeType.PERSIST, CascadeType.MERGE })
+    @Cascade( { org.hibernate.annotations.CascadeType.SAVE_UPDATE,
+	    org.hibernate.annotations.CascadeType.DELETE_ORPHAN })
+    private List<CampaignMetric> campaignMetric;
 
-	public Integer getId() {
-		return id;
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	final Campaign other = (Campaign) obj;
+	if (id == null) {
+	    if (other.id != null)
+		return false;
+	} else if (!id.equals(other.id))
+	    return false;
+	return true;
+    }
 
-	public Double getMinimumValue() {
-		return minimumValue;
-	}
+    public List<Agent> getAgents() {
+	return agents;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public List<Supervisor> getSupervisors() {
+	return supervisors;
+    }
 
-	public Double getObjetiveValue() {
-		return objetiveValue;
-	}
+    public List<CampaignMetric> getCampaignMetric() {
+	return campaignMetric;
+    }
 
-	/*
-	 * public List<Metric> getMetrics() { return metrics; }
-	 * 
-	 * public void setMetrics(List<Metric> metrics) { this.metrics = metrics; }
-	 */
+    // public String getCode() {
+    // return code;
+    // }
 
-	public Double getOptimValue() {
-		return optimValue;
-	}
+    public Integer getId() {
+	return id;
+    }
 
-	public CampaignType getType() {
-		return type;
-	}
+    public Double getMinimumValue() {
+	return minimumValue;
+    }
 
-	public Double getUnsatisfactoryValue() {
-		return unsatisfactoryValue;
-	}
+    public String getName() {
+	return name;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public Double getObjetiveValue() {
+	return objetiveValue;
+    }
 
-	public void setAgents(List<Agent> agents) {
-		this.agents = agents;
-	}
-	
-	public void setSupervisors(List<Supervisor> supervisors) {
-		this.supervisors = supervisors;
-	}
+    /*
+     * public List<Metric> getMetrics() { return metrics; }
+     * 
+     * public void setMetrics(List<Metric> metrics) { this.metrics = metrics; }
+     */
 
-	public void setCampaignMetric(List<CampaignMetric> campaignMetric) {
-		this.campaignMetric = campaignMetric;
-	}
+    public Double getOptimValue() {
+	return optimValue;
+    }
 
-//	public void setCode(String code) {
-//		this.code = code;
-//	}
+    public CampaignType getType() {
+	return type;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Double getUnsatisfactoryValue() {
+	return unsatisfactoryValue;
+    }
 
-	public void setMinimumValue(Double minimumValue) {
-		this.minimumValue = minimumValue;
-	}
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	return result;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setAgents(List<Agent> agents) {
+	this.agents = agents;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setSupervisors(List<Supervisor> supervisors) {
+	this.supervisors = supervisors;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setCampaignMetric(List<CampaignMetric> campaignMetric) {
+	this.campaignMetric = campaignMetric;
+    }
 
-	public void setObjetiveValue(Double objetiveValue) {
-		this.objetiveValue = objetiveValue;
-	}
+    // public void setCode(String code) {
+    // this.code = code;
+    // }
 
-	public void setOptimValue(Double optimValue) {
-		this.optimValue = optimValue;
-	}
+    public void setId(Integer id) {
+	this.id = id;
+    }
 
-	public void setType(CampaignType type) {
-		this.type = type;
-	}
+    public void setMinimumValue(Double minimumValue) {
+	this.minimumValue = minimumValue;
+    }
 
-	public void setUnsatisfactoryValue(Double unsatisfactoryValue) {
-		this.unsatisfactoryValue = unsatisfactoryValue;
-	}
+    public void setName(String name) {
+	this.name = name;
+    }
 
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
+    public void setDescription(String description) {
+	this.description = description;
+    }
 
-	public Date getStartDate() {
-		return startDate;
-	}
+    public String getDescription() {
+	return description;
+    }
 
-	public Date getEndDate() {
-		return endDate;
-	}
-	
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-	
+    public void setObjetiveValue(Double objetiveValue) {
+	this.objetiveValue = objetiveValue;
+    }
+
+    public void setOptimValue(Double optimValue) {
+	this.optimValue = optimValue;
+    }
+
+    public void setType(CampaignType type) {
+	this.type = type;
+    }
+
+    public void setUnsatisfactoryValue(Double unsatisfactoryValue) {
+	this.unsatisfactoryValue = unsatisfactoryValue;
+    }
+
+    public void setStartDate(Date startDate) {
+	this.startDate = startDate;
+    }
+
+    public Date getStartDate() {
+	return startDate;
+    }
+
+    public Date getEndDate() {
+	return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+	this.endDate = endDate;
+    }
+
     @Override
     public String toString() {
-    	if(this.id == null)
-    		return "";
-    	return this.id.toString() + " - " + this.name;
+	if (this.id == null)
+	    return "";
+	return this.id.toString() + " - " + this.name;
     }
 
 }
