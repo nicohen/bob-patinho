84b4b7698cf15ceb88b93f082c494480ca157ac6
duilio.hernandez@gmail.com <duilio.hernandez@gmail.com@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-10-21 18:06:44 +0000

    TK-01035 - métrica AUX_TM

--- trunk/code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/SummaryDaoImpl.java
+++ trunk/code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/SummaryDaoImpl.java
@@ -1,24 +1,43 @@
 package com.self_managment.persistance.dao.impl;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-
+import com.self_managment.model.entity.Agent;
 import com.self_managment.model.entity.Summary;
-
 import com.self_managment.persistance.dao.SummaryDao;
 
-
 @Repository("summaryDao")
-public class SummaryDaoImpl extends GenericDaoImpl<Summary, Serializable> implements
-		SummaryDao {
+public class SummaryDaoImpl extends GenericDaoImpl<Summary, Serializable>
+		implements SummaryDao {
 
 	@Override
 	protected Class<Summary> getEntityClass() {
 		return Summary.class;
 	}
-	
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public Long getAmountOfTimeInAnAvailableCallStatus(Agent agent) {
+		Calendar cal = Calendar.getInstance();
+		Date today = new Date();
+		cal.setTime(today);
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+
+		List<Long> result = getHibernateTemplate()
+				.find(
+						"select sum(inCall) from Summary where pk.agent=? and pk.date between ? and ?",
+						new Object[] { agent, cal.getTime(), today });
+
+		return result.get(0) != null ? result.get(0) : 0L;
+
+	}
 
 }
---
+++ trunk/code/self_managment/src/test/java/com/self_managment/metric/MetricAmountOfTimeInAnAvailableCallStatusTest.java
@@ -0,0 +1,46 @@
+package com.self_managment.metric;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.self_managment.model.entity.Agent;
+import com.self_managment.model.metric.MetricStrategy;
+
+public class MetricAmountOfTimeInAnAvailableCallStatusTest extends TestCase {
+
+	private MetricStrategy metric;
+
+	public static Test suite() {
+		return new TestSuite(
+				MetricAmountOfTimeInAnAvailableCallStatusTest.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext(
+				"spring/config/beanlocations.xml");
+
+		metric = (MetricStrategy) appContext.getBean("AVAIL_TM");
+	}
+
+	public void testMetricWithResult() {
+		Agent agent = new Agent();
+		agent.setDocket(1);
+		assertEquals(8L, metric.execute(agent));
+	}
+
+	public void testMetricWithoutResult() {
+		Agent agent = new Agent();
+		agent.setDocket(999);
+		assertEquals(0L, metric.execute(agent));
+	}
+
+	public void testMetricWithNullAgent() {
+		assertNull(metric.execute(null));
+	}
+
+}
