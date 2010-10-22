e3d75d14cfc003c59890a8a82debbd8ad7fc2145
duilio.hernandez <duilio.hernandez@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-10-22 01:41:58 +0000

    S-01020 : se muestra la métrica en la pantalla

--- trunk/code/self_managment_web/src/main/java/com/self_managment/web/controller/GraficWebController.java
+++ trunk/code/self_managment_web/src/main/java/com/self_managment/web/controller/GraficWebController.java
@@ -7,197 +7,182 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
-import javax.faces.context.FacesContext;
+import javax.faces.component.html.HtmlOutputText;
 import javax.imageio.ImageIO;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.time.Day;
 import org.jfree.data.time.TimeSeriesCollection;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import org.springframework.web.jsf.FacesContextUtils;
 
 import com.self_managment.model.entity.Agent;
 import com.self_managment.model.entity.CampaignMetric;
 import com.self_managment.service.AgentService;
 import com.self_managment.service.MetricService;
 import com.self_managment.service.QAService;
+import com.self_managment.web.util.JSFUtil;
 
 @Component("graficBean")
 @Scope("session")
 public class GraficWebController {
-	public String metric1 = "QA_MONITORS";
-	public String metric2 = "QA_PTS_ACHIEVED";
-	public String metric3 = "QA_PTS_POSSIBLE";
+    public Integer metric1_optim = 80;
+    public Integer metric1_objetivo = 50;
+    public Integer metric1_minimo = 20;
+    public Integer metric1_noSatisfactorio = 10;
+    public Integer metric2_optim = 50;
+    public Integer metric2_objetivo = 35;
+    public Integer metric2_minimo = 15;
+    public Integer metric2_noSatisfactorio = 5;
+    public Integer metric3_optim = 35;
+    public Integer metric3_objetivo = 20;
+    public Integer metric3_minimo = 10;
+    public Integer metric3_noSatisfactorio = 5;
 
-	public Integer metric1_optim = 80;
-	public Integer metric1_objetivo = 50;
-	public Integer metric1_minimo = 20;
-	public Integer metric1_noSatisfactorio = 10;
-	public Integer metric2_optim = 50;
-	public Integer metric2_objetivo = 35;
-	public Integer metric2_minimo = 15;
-	public Integer metric2_noSatisfactorio = 5;
-	public Integer metric3_optim = 35;
-	public Integer metric3_objetivo = 20;
-	public Integer metric3_minimo = 10;
-	public Integer metric3_noSatisfactorio = 5;
+    public double value_metrics_optim = 20.93;
+    public double value_metrics_objetivo = 15.65;
+    public double value_metrics_minimo = 10.95;
+    public double value_metrics_noSatisfactorio = 0.00;
 
-	public double value_metrics_optim = 20.93;
-	public double value_metrics_objetivo = 15.65;
-	public double value_metrics_minimo = 10.95;
-	public double value_metrics_noSatisfactorio = 0.00;
+    public Integer metric1_values = 89;
+    public Integer metric2_values = 47;
+    public Integer metric3_values = 32;
 
-	public Integer metric1_values = 89;
-	public Integer metric2_values = 47;
-	public Integer metric3_values = 32;
+    public Integer cantHoras = 108;
 
-	public Integer cantHoras = 108;
+    private HtmlOutputText metricOutput;
+    private String today = now();
+    private double sueldoFijo;
+    private double sueldoVariableProyectado;
+    private Integer metricaCalculadaProy;
+    private List<CampaignMetric> metrics;
 
-	private String today = now();
-	private String currentCampaign;
-	private double sueldoFijo;
-	private double sueldoVariableProyectado;
-	private Integer metricaCalculadaProy;
-	private List<CampaignMetric> metrics;
-	private String agentName;
+    AgentService agentService = (AgentService) JSFUtil.getBean("agentService");
+    QAService qaService = (QAService) JSFUtil.getBean("qaService");
+    MetricService metricService = (MetricService) JSFUtil
+	    .getBean("metricService");
 
-	ApplicationContext ctx = FacesContextUtils
-			.getWebApplicationContext(FacesContext.getCurrentInstance());
+    Agent currentAgent = agentService.findById(1);
 
-	AgentService agentService = (AgentService) ctx.getBean("agentService");
-	QAService qaService = (QAService) ctx.getBean("qaService");
-	MetricService metricService = (MetricService) ctx.getBean("metricService");
+    org.jfree.data.time.TimeSeries pop = new org.jfree.data.time.TimeSeries(
+	    "Linea de Crecimiento");
 
-	Agent currentAgent = agentService.findById(1);
+    Integer CantDias = 0;
 
-	Double QAPossiblePoints;
+    public void generaGrafico(OutputStream out, Object data) throws IOException {
 
-	org.jfree.data.time.TimeSeries pop = new org.jfree.data.time.TimeSeries(
-			"Linea de Crecimiento");
+	pop.add(new Day(1, 10, 2010), 0);
+	pop.add(new Day(18, 10, 2010), sueldoALaFecha());
+	pop.add(new Day(30, 10, 2010), sueldoProyectado());
 
-	Integer CantDias = 0;
+	TimeSeriesCollection dataset = new TimeSeriesCollection();
+	dataset.addSeries(pop);
 
-	public void generaGrafico(OutputStream out, Object data) throws IOException {
+	JFreeChart chart1 = ChartFactory.createTimeSeriesChart("Tendencia",
+		"Dias", "Proyeccion Metrica", dataset, true, true, false);
 
-		pop.add(new Day(1, 10, 2010), 0);
-		pop.add(new Day(18, 10, 2010), sueldoALaFecha());
-		pop.add(new Day(30, 10, 2010), sueldoProyectado());
+	BufferedImage buffImg = chart1.createBufferedImage(500, // width
+		375, // height
+		BufferedImage.TYPE_INT_RGB, // image type
+		null);
 
-		TimeSeriesCollection dataset = new TimeSeriesCollection();
-		dataset.addSeries(pop);
+	ImageIO.write(buffImg, "jpeg", out);
 
-		JFreeChart chart1 = ChartFactory.createTimeSeriesChart("Tendencia",
-				"Dias", "Proyeccion Metrica", dataset, true, true, false);
+    }
 
-		BufferedImage buffImg = chart1.createBufferedImage(500, // width
-				375, // height
-				BufferedImage.TYPE_INT_RGB, // image type
-				null);
+    public Agent getCurrentAgent() {
+	return currentAgent;
+    }
 
-		ImageIO.write(buffImg, "jpeg", out);
+    public Integer getMetric1_optim() {
+	return metric1_optim;
+    }
 
+    public Integer getMetricCalculateProy() {
+	// metricaCalculadaProy = (30 - CantDias) * sumaTotal;
+	return metricaCalculadaProy;
+    }
+
+    public HtmlOutputText getMetricOutput() {
+	return metricOutput;
+    }
+
+    public List<CampaignMetric> getMetrics() {
+	if (metrics == null) {
+	    metrics = currentAgent.getCampaign().getCampaignMetric();
 	}
+	return metrics;
+    }
 
-	private String now() {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-		return sdf.format(Calendar.getInstance().getTime());
-	}
+    public String getMetricValue() {
+	CampaignMetric metric = (CampaignMetric) metricOutput.getAttributes()
+		.get("metric");
+	return metric.getMetric().execute(currentAgent).toString();
+    }
 
-	public double sueldoALaFecha() {
-		if (metric1_values > metric1_optim && metric1_values > metric2_optim
-				&& metric1_values > metric3_optim)
-			sueldoFijo = cantHoras * value_metrics_optim;
-		return sueldoFijo;
-	}
+    public double getSueldoFijo() {
+	return currentAgent.getGrossSalary();
+    }
 
-	public double sueldoProyectado() {
-		double proyectado_metric1;
-		double proyectado_metric2;
-		double proyectado_metric3;
+    public double getSueldoProyectado() {
+	sueldoVariableProyectado = sueldoProyectado();
+	return sueldoVariableProyectado;
+    }
 
-		// List<QA> qas = qaService.sumPossiblePoints("1");
-		// qas.get(0).getPk().getAgent().getGrossSalary()
+    public double getSueldoVariableProyectado() {
+	return sueldoVariableProyectado;
+    }
 
-		proyectado_metric1 = (160 * metric1_values) / cantHoras;
-		proyectado_metric2 = (160 * metric2_values) / cantHoras;
-		proyectado_metric3 = (160 * metric3_values) / cantHoras;
+    public String getToday() {
+	return today;
+    }
 
-		if (proyectado_metric1 > metric1_optim
-				&& proyectado_metric2 > metric2_optim
-				&& proyectado_metric3 > metric3_optim)
-			sueldoVariableProyectado = 160 * value_metrics_optim;
-		return sueldoVariableProyectado;
-	}
+    private String now() {
+	SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+	return sdf.format(Calendar.getInstance().getTime());
+    }
 
-	public Integer getMetric1_optim() {
-		return metric1_optim;
-	}
+    public void setCurrentAgent(Agent currentAgent) {
+	this.currentAgent = currentAgent;
+    }
 
-	public double getSueldoProyectado() {
-		sueldoVariableProyectado = sueldoProyectado();
-		return sueldoVariableProyectado;
-	}
+    public void setMetricOutput(HtmlOutputText metricOutput) {
+	this.metricOutput = metricOutput;
+    }
 
-	public void setToday(String today) {
-		this.today = today;
-	}
+    public void setSueldoFijo(double sueldoFijo) {
+	this.sueldoFijo = sueldoFijo;
+    }
 
-	public String getToday() {
-		return today;
-	}
+    public void setSueldoVariableProyectado(double sueldoVariableProyectado) {
+	this.sueldoVariableProyectado = sueldoVariableProyectado;
+    }
 
-	public void setCurrentCampaign(String currentCampaign) {
-		this.currentCampaign = currentCampaign;
-	}
+    public double sueldoALaFecha() {
+	if (metric1_values > metric1_optim && metric1_values > metric2_optim
+		&& metric1_values > metric3_optim)
+	    sueldoFijo = cantHoras * value_metrics_optim;
+	return sueldoFijo;
+    }
 
-	public String getCurrentCampaign() {
-		if (this.currentCampaign == null) {
-			this.currentCampaign = currentAgent.getCampaign().getName();
-		}
-		return this.currentCampaign;
-	}
+    public double sueldoProyectado() {
+	double proyectado_metric1;
+	double proyectado_metric2;
+	double proyectado_metric3;
 
-	public void setSueldoFijo(double sueldoFijo) {
-		this.sueldoFijo = sueldoFijo;
-	}
+	// List<QA> qas = qaService.sumPossiblePoints("1");
+	// qas.get(0).getPk().getAgent().getGrossSalary()
 
-	public double getSueldoFijo() {
-		return currentAgent.getGrossSalary();
-	}
+	proyectado_metric1 = (160 * metric1_values) / cantHoras;
+	proyectado_metric2 = (160 * metric2_values) / cantHoras;
+	proyectado_metric3 = (160 * metric3_values) / cantHoras;
 
-	public void setSueldoVariableProyectado(double sueldoVariableProyectado) {
-		this.sueldoVariableProyectado = sueldoVariableProyectado;
-	}
-
-	public double getSueldoVariableProyectado() {
-		return sueldoVariableProyectado;
-	}
-
-	public void setMetrics(List<CampaignMetric> metrics) {
-		this.metrics = metrics;
-	}
-
-	public List<CampaignMetric> getMetrics() {
-		if (metrics == null) {
-			metrics = currentAgent.getCampaign().getCampaignMetric();
-		}
-		return metrics;
-	}
-
-	public void setAgentName(String agentName) {
-		this.agentName = agentName;
-	}
-
-	public String getAgentName() {
-		return currentAgent.getName();
-	}
-
-	public Integer getMetricCalculateProy() {
-//		metricaCalculadaProy = (30 - CantDias) * sumaTotal;
-		return metricaCalculadaProy;
-	}
+	if (proyectado_metric1 > metric1_optim
+		&& proyectado_metric2 > metric2_optim
+		&& proyectado_metric3 > metric3_optim)
+	    sueldoVariableProyectado = 160 * value_metrics_optim;
+	return sueldoVariableProyectado;
+    }
 
 }
