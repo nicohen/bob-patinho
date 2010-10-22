966f563ac45f3fdf157763b73d1a4d391c3cced3
duilio.hernandez <duilio.hernandez@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-10-22 01:41:58 +0000

    S-01020 : se muestra la métrica en la pantalla

--- code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/QADaoImpl.java
+++ code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/QADaoImpl.java
@@ -8,25 +8,35 @@ import com.self_managment.model.entity.QA;
 import com.self_managment.persistance.dao.QADao;
 
 @Repository("qaDao")
-public class QADaoImpl extends GenericDaoImpl<QA, Serializable> implements QADao {
+public class QADaoImpl extends GenericDaoImpl<QA, Serializable> implements
+	QADao {
 
-	@Override
-	protected Class<QA> getEntityClass() {
-		return QA.class;
-	}
-	
-	public Double sumPossiblePoints(Integer docket) {
-		//select sum(puntos_posibles) from qa where legajo=? and fecha between ? and ? // fecha desde = primer día del mes, fecha hasta = hoy
-		return (Double)getHibernateTemplate().find("select sum(posible_points_quantity) from qa where docket=?",docket).get(0);
-	}
-	
-	public Double sumQAMonitors(Integer docket) {
-		//select count(1) from qa where legajo=? and fecha between ? and ? // fecha desde = primer día del mes, fecha hasta = hoy
-		return (Double)getHibernateTemplate().find("select count(1) from qa where docket=?",docket).get(0);
-	}
+    @Override
+    protected Class<QA> getEntityClass() {
+	return QA.class;
+    }
 
-	public Number sumAchievedPoints(Integer docket) {
-		//select sum(achieved_points_quantity) from qa where legajo=? and fecha between ? and ? // fecha desde = primer día del mes, fecha hasta = hoy
-		return (Double)getHibernateTemplate().find("select sum(achieved_points_quantity) from qa where docket=?",docket).get(0);
-	}
+    public Double sumPossiblePoints(Integer docket) {
+	// select sum(puntos_posibles) from qa where legajo=? and fecha between
+	// ? and ? // fecha desde = primer dia del mes, fecha hasta = hoy
+	return (Double) getHibernateTemplate().find(
+		"select sum(posible_points_quantity) from qa where docket=?",
+		docket).get(0);
+    }
+
+    public Double sumQAMonitors(Integer docket) {
+	// select count(1) from qa where legajo=? and fecha between ? and ? //
+	// fecha desde = primer dia del mes, fecha hasta = hoy
+	return (Double) getHibernateTemplate().find(
+		"select count(1) from qa where docket=?", docket).get(0);
+    }
+
+    public Number sumAchievedPoints(Integer docket) {
+	// select sum(achieved_points_quantity) from qa where legajo=? and fecha
+	// between ? and ? // fecha desde = primer dia del mes, fecha hasta =
+	// hoy
+	return (Double) getHibernateTemplate().find(
+		"select sum(achieved_points_quantity) from qa where docket=?",
+		docket).get(0);
+    }
 }
