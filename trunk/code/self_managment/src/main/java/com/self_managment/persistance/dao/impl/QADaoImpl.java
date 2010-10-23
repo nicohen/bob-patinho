29ddaddcd3f82fa46708d882067ec028a7c88315
nicohen <nicohen@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-10-23 00:07:34 +0000

    S-01020 - QA_MONITORS, QA_PTS_POSSIBLE, QA_PTS_ACHIEVED finalizadas

--- trunk/code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/QADaoImpl.java
+++ trunk/code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/QADaoImpl.java
@@ -1,6 +1,9 @@
 package com.self_managment.persistance.dao.impl;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
@@ -13,30 +16,64 @@ public class QADaoImpl extends GenericDaoImpl<QA, Serializable> implements
 
     @Override
     protected Class<QA> getEntityClass() {
-	return QA.class;
+    	return QA.class;
     }
 
-    public Double sumPossiblePoints(Integer docket) {
-	// select sum(puntos_posibles) from qa where legajo=? and fecha between
-	// ? and ? // fecha desde = primer dia del mes, fecha hasta = hoy
-	return (Double) getHibernateTemplate().find(
-		"select sum(posible_points_quantity) from qa where docket=?",
-		docket).get(0);
+    @SuppressWarnings("unchecked")
+    @Override
+    public Number sumPossiblePoints(Integer docket) {
+		Calendar cal = Calendar.getInstance();
+		Date today = new Date();
+		cal.setTime(today);
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+
+		List<Long> result = getHibernateTemplate().find(
+    		"select sum(posible_points_quantity) from qa where docket=? and date between ? and ?",
+    		new Object[] { docket, cal.getTime(), today });
+	    
+		return result.get(0) != null ? result.get(0) : 0L;
+
     }
 
-    public Double sumQAMonitors(Integer docket) {
-	// select count(1) from qa where legajo=? and fecha between ? and ? //
-	// fecha desde = primer dia del mes, fecha hasta = hoy
-	return (Double) getHibernateTemplate().find(
-		"select count(1) from qa where docket=?", docket).get(0);
+    @SuppressWarnings("unchecked")
+    @Override
+    public Number sumQAMonitors(Integer docket) {
+		Calendar cal = Calendar.getInstance();
+		Date today = new Date();
+		cal.setTime(today);
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+	
+		List<Long> result = getHibernateTemplate().find(
+			"select count(1) from qa where docket=? and date between ? and ?", 
+			new Object[] { docket, cal.getTime(), today });
+	    
+		return result.get(0) != null ? result.get(0) : 0L;
+
     }
 
-    public Number sumAchievedPoints(Integer docket) {
-	// select sum(achieved_points_quantity) from qa where legajo=? and fecha
-	// between ? and ? // fecha desde = primer dia del mes, fecha hasta =
-	// hoy
-	return (Double) getHibernateTemplate().find(
-		"select sum(achieved_points_quantity) from qa where docket=?",
-		docket).get(0);
+    @SuppressWarnings("unchecked")
+    @Override
+	public Number sumAchievedPoints(Integer docket) {
+		Calendar cal = Calendar.getInstance();
+		Date today = new Date();
+		cal.setTime(today);
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+	
+		List<Long> result = getHibernateTemplate().find(
+			"select sum(achieved_points_quantity) from qa where docket=? and date between ? and ?",
+			new Object[] { docket, cal.getTime(), today });
+	    
+		return result.get(0) != null ? result.get(0) : 0L;
+
     }
+
 }
