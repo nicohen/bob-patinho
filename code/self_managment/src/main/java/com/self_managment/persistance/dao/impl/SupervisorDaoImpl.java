1e3af4eff7a8c9674b3e3435f8b32738a846b528
duilio.hernandez <duilio.hernandez@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-10-24 01:10:52 +0000

    remove onload

--- code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/SupervisorDaoImpl.java
+++ code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/SupervisorDaoImpl.java
@@ -2,7 +2,7 @@ package com.self_managment.persistance.dao.impl;
 
 import java.io.Serializable;
 import java.util.List;
-import org.hibernate.criterion.Expression;
+
 import org.springframework.stereotype.Repository;
 
 import com.self_managment.model.entity.Supervisor;
@@ -10,16 +10,19 @@ import com.self_managment.persistance.dao.SupervisorDao;
 
 @Repository("supervisorDao")
 public class SupervisorDaoImpl extends GenericDaoImpl<Supervisor, Serializable>
-		implements SupervisorDao {
+	implements SupervisorDao {
 
-	@Override
-	protected Class<Supervisor> getEntityClass() {
-		return Supervisor.class;
-	}
-	@Override
-	public List<Supervisor> findAllSupervisorsWithoutCampaign()
-	{
-		return this.getSession().createCriteria(Supervisor.class).add(Expression.isNull("campaign")).list();
-	}
+    @Override
+    protected Class<Supervisor> getEntityClass() {
+	return Supervisor.class;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Supervisor> findAllSupervisorsWithoutCampaign() {
+	return getHibernateTemplate().find("from Supervisor where campaign is null");
+	// return
+	// this.getSession().createCriteria(Supervisor.class).add(Expression.isNull("campaign")).list();
+    }
 
 }
