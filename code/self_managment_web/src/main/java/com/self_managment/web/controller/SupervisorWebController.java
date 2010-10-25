68c96d810dc23bb1cb573fc16f6c2590c1b18fbb
duilio.hernandez@gmail.com <duilio.hernandez@gmail.com@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-10-25 19:27:16 +0000

    S-01022 - se modifican cascades de hibernate. Se agrega atributo refresh en la pantalla supervisor para actualizar datos de la session (campañas y supervisores)

--- code/self_managment_web/src/main/java/com/self_managment/web/controller/SupervisorWebController.java
+++ code/self_managment_web/src/main/java/com/self_managment/web/controller/SupervisorWebController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.faces.model.SelectItem;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -15,80 +16,84 @@ import com.self_managment.web.util.JSFUtil;
 @Component("supervisorBean")
 @Scope("session")
 public class SupervisorWebController {
-    private List<Supervisor> supervisors;
-    private List<SelectItem> campaigns;
-    private Supervisor supervisor = new Supervisor();
-    private SupervisorService service;
-    private CampaignService campaignService;
-    private boolean editMode;
+	private List<Supervisor> supervisors;
+	private List<SelectItem> campaigns;
+	private Supervisor supervisor = new Supervisor();
+	private SupervisorService service;
+	private CampaignService campaignService;
+	private boolean editMode;
 
-    public SupervisorWebController() {
-	service = (SupervisorService) JSFUtil.getBean("supervisorService");
-	campaignService = (CampaignService) JSFUtil.getBean("campaignService");
-    }
-
-    public void onload() {
-	supervisors = service.findAll();
-    }
-
-    public List<Supervisor> getSupervisors() {
-	if (supervisors == null)
-	    supervisors = service.findAll();
-	return supervisors;
-    }
-
-    public List<SelectItem> getCampaigns() {
-	if (campaigns == null)
-	    campaigns = JSFUtil.getSelectItems(campaignService.findAll());
-	return campaigns;
-    }
-
-    public Supervisor getSupervisor() {
-	return supervisor;
-    }
-
-    public String update() {
-	try {
-	    if (!editMode)
-		service.save(supervisor);
-	    else
-		service.update(supervisor);
-	    setSupervisor(new Supervisor());
-	} catch (Exception e) {
-	    JSFUtil.addErrorMessage("Error");
-	} finally {
-	    supervisors = service.findAll();
+	public SupervisorWebController() {
+		service = (SupervisorService) JSFUtil.getBean("supervisorService");
+		campaignService = (CampaignService) JSFUtil.getBean("campaignService");
 	}
-	return "";
-    }
 
-    public void setSupervisor(Supervisor supervisor) {
-	this.supervisor = supervisor;
-    }
-
-    public String create() {
-	editMode = false;
-	setSupervisor(new Supervisor());
-	return "";
-    }
-
-    public String delete() {
-	try {
-	    service.delete(supervisor);
-	} catch (Exception e) {
-	    JSFUtil.addErrorMessage("Error");
-	} finally {
-	    supervisors = service.findAll();
+	public void onload() {
+		supervisors = service.findAll();
 	}
-	return "";
-    }
 
-    public boolean isEditMode() {
-	return editMode;
-    }
+	public List<Supervisor> getSupervisors() {
+		String refresh = StringUtils.trimToEmpty(JSFUtil
+				.getRequestParameter("refresh"));
+		if (supervisors == null || "true".equals(refresh))
+			supervisors = service.findAll();
+		return supervisors;
+	}
 
-    public void setEditMode() {
-	this.editMode = true;
-    }
+	public List<SelectItem> getCampaigns() {
+		String refresh = StringUtils.trimToEmpty(JSFUtil
+				.getRequestParameter("refresh"));
+		if (campaigns == null || "true".equals(refresh))
+			campaigns = JSFUtil.getSelectItems(campaignService.findAll());
+		return campaigns;
+	}
+
+	public Supervisor getSupervisor() {
+		return supervisor;
+	}
+
+	public String update() {
+		try {
+			if (!editMode)
+				service.save(supervisor);
+			else
+				service.update(supervisor);
+			setSupervisor(new Supervisor());
+		} catch (Exception e) {
+			JSFUtil.addErrorMessage("Error");
+		} finally {
+			supervisors = service.findAll();
+		}
+		return "";
+	}
+
+	public void setSupervisor(Supervisor supervisor) {
+		this.supervisor = supervisor;
+	}
+
+	public String create() {
+		editMode = false;
+		setSupervisor(new Supervisor());
+		return "";
+	}
+
+	public String delete() {
+		try {
+			service.delete(supervisor);
+		} catch (Exception e) {
+			JSFUtil.addErrorMessage("Error");
+		} finally {
+			supervisors = service.findAll();
+		}
+		return "";
+	}
+
+	public boolean isEditMode() {
+		return editMode;
+	}
+
+	public void setEditMode() {
+		this.editMode = true;
+	}
 
 }
