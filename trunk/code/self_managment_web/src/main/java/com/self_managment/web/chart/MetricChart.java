866ecbf71f659f21a209daf3c0352b2011f01855
nicohen <nicohen@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-10-31 18:38:34 +0000

    S-01020 - Calculo de la recta de cuadrados minimos

--- trunk/code/self_managment_web/src/main/java/com/self_managment/web/chart/MetricChart.java
+++ trunk/code/self_managment_web/src/main/java/com/self_managment/web/chart/MetricChart.java
@@ -3,16 +3,27 @@ package com.self_managment.web.chart;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.ResourceBundle;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.jfree.chart.ChartColor;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.Marker;
+import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.ValueMarker;
+import org.jfree.data.statistics.Statistics;
 import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.RectangleAnchor;
 import org.jfree.ui.TextAnchor;
 
@@ -35,8 +46,8 @@ public class MetricChart {
     public BufferedImage createBufferedImage(int width, int height) {
 
 	JFreeChart chart = ChartFactory.createTimeSeriesChart(campaignMetric
-		.getMetric().getCode().toUpperCase(), "DÃ­as",
-		"ProyecciÃ³n MÃ©trica", getDataSet(), true, true, false);
+		.getMetric().getCode().toUpperCase(), "Días",
+		"Proyección Métrica", getDataSet(), true, true, false);
 
 	ResourceBundle rb = ResourceBundle.getBundle("messages");
 
@@ -60,27 +71,60 @@ public class MetricChart {
     }
 
     private TimeSeriesCollection getDataSet() {
-	org.jfree.data.time.TimeSeries pop = new org.jfree.data.time.TimeSeries(
-		"Puntos Acumulados");
-
+	TimeSeries pop = new TimeSeries("Puntos Acumulados");
+	TimeSeries popLeastSquares = new TimeSeries("Proyección");
+	SortedSet<Day> acumDays = new TreeSet<Day>();
+	
 	Date dateTo = DateUtils.getLastDay(period);
 	Date date = DateUtils.getFirstDay(period);
+	Date leastSquaresFirstDate = (Date) date.clone();
 
 	double acum = 0;
 	while (date.before(dateTo)) {
 	    acum += campaignMetric.getMetric().execute(agent, date, date)
 		    .doubleValue();
 	    pop.add(new Day(date), acum);
+	    acumDays.add(new Day(date));
 	    date = org.apache.commons.lang.time.DateUtils.addDays(date, 1);
 	}
 
+	double[] linearFit = getLinearFit(acumDays,pop);
+	double startY = getY(linearFit[0],linearFit[1],date);
+	double endY = getY(linearFit[0],linearFit[1],dateTo);
+	popLeastSquares.addOrUpdate(new Day(leastSquaresFirstDate),startY);
+	popLeastSquares.addOrUpdate(new Day(dateTo),endY);
+	System.out.println("linearFit[0]:"+linearFit[0]+" # linearFit[1]:"+linearFit[1]+" # dayOfMonthFrom:"+new Day(date)+" # startY:"+startY);
+	System.out.println("linearFit[0]:"+linearFit[0]+" # linearFit[1]:"+linearFit[1]+" # dayOfMonthTo:"+new Day(dateTo).getDayOfMonth()+" # endY:"+endY);
+	
 	TimeSeriesCollection dataset = new TimeSeriesCollection();
 	dataset.addSeries(pop);
+	dataset.addSeries(popLeastSquares);
 
 	return dataset;
     }
 
-    private Marker getMarker(double value, Color color, String label) {
+    private double getY(double b, double a, Date date) {
+    	// Y = aX + b
+    	Day d = new Day(date);
+    	return (d.getDayOfMonth()*a)+b;
+	}
+
+	private double[] getLinearFit(SortedSet<Day> acumDays, TimeSeries pop) {
+    	Iterator<Day> it = acumDays.iterator();
+    	Double[] xValues = new Double[acumDays.size()];
+    	Double[] yValues = new Double[acumDays.size()];
+    	int i=0;
+    	
+    	while(it.hasNext()) {
+    		Day d = it.next();
+    		xValues[i] = new Double(d.getDayOfMonth()); 
+    		yValues[i++] = (Double) pop.getValue(d);
+    	}
+    	
+    	return Statistics.getLinearFit(xValues, yValues);
+	}
+
+	private Marker getMarker(double value, Color color, String label) {
 	final Marker marker = new ValueMarker(value);
 	marker.setPaint(color);
 	marker.setLabel(label);
@@ -89,6 +133,44 @@ public class MetricChart {
 	marker.setStroke(new BasicStroke(3.f));
 
 	return marker;
+    }
+
+    
+    public static void main(String[] args) {
+        //         Create a simple XY chart
+    	XYSeries series = new XYSeries("XYGraph");
+        series.add(1, 1);
+        series.add(1, 2);
+        series.add(2, 1);
+        series.add(3, 9);
+        series.add(4, 10);
+    	XYSeries series2 = new XYSeries("XYGraph2");
+        series2.add(1, 1);
+        series2.add(4, 2);
+        series2.add(5, 1);
+        series2.add(6, 9);
+        series2.add(7, 10);
+        //         Add the series to your data set
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+        dataset.addSeries(series2);
+        //         Generate the graph
+        JFreeChart chart = ChartFactory.createXYLineChart("XY Chart", // Title
+                "x-axis", // x-axis Label
+                "y-axis", // y-axis Label
+                dataset, // Dataset
+                PlotOrientation.VERTICAL, // Plot Orientation
+                true, // Show Legend
+                true, // Use tooltips
+                false // Configure chart to generate URLs?
+            );
+        
+        try {
+            ChartUtilities.saveChartAsJPEG(new File("C:chart.jpg"), chart, 500,
+                300);
+        } catch (IOException e) {
+            System.err.println("Problem occurred creating chart.");
+        }
     }
 
 }
