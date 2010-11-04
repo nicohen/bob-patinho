c42a4d86c4e47024f2665a599a28500c698993f9
sergiocolinas <sergiocolinas@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-11-04 01:32:23 +0000

    Se modifica la firma del metodo getProductiveHours para que reciba fecha desde y fecha hasta

--- code/self_managment/src/main/java/com/self_managment/persistance/dao/TTSDao.java
+++ code/self_managment/src/main/java/com/self_managment/persistance/dao/TTSDao.java
@@ -1,13 +1,14 @@
 package com.self_managment.persistance.dao;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import com.self_managment.model.entity.TTS;
 
 public interface TTSDao extends GenericDao<TTS, Serializable> {
 	
-	public List<TTS> findByAgentMonthYear(Integer docket, Integer month, Integer year);
+	public List<TTS> findByAgentDateFromDateTo(Integer docket, Date dateFrom, Date dateTo);
 
 }
 
--- code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/TTSDaoImpl.java
+++ code/self_managment/src/main/java/com/self_managment/persistance/dao/impl/TTSDaoImpl.java
@@ -1,13 +1,11 @@
 package com.self_managment.persistance.dao.impl;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
-
 import org.springframework.stereotype.Repository;
-
 import com.self_managment.model.entity.TTS;
 import com.self_managment.persistance.dao.TTSDao;
-import com.self_managment.util.DateUtils;
 
 @Repository("ttsDao")
 public class TTSDaoImpl extends GenericDaoImpl<TTS, Serializable> implements
@@ -17,15 +15,13 @@ public class TTSDaoImpl extends GenericDaoImpl<TTS, Serializable> implements
     protected Class<TTS> getEntityClass() {
 	return TTS.class;
     }
-
+    
     @SuppressWarnings("unchecked")
-    public List<TTS> findByAgentMonthYear(Integer docket, Integer month,
-	    Integer year) {
+    public List<TTS> findByAgentDateFromDateTo(Integer docket, Date dateFrom, Date dateTo) {
 
 	return getHibernateTemplate().find(
 		"from TTS where pk.agent.docket=? and bulgingDate > ? and pk.date <= ?",
-		new Object[] { docket, DateUtils.getFirstDay(month, year),
-			DateUtils.getLastDay(month, year) });
+		new Object[] { docket, dateFrom,dateTo });
     }
 
 }
