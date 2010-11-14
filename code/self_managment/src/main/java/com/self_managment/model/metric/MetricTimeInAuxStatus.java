e9ef2e543b2e920bac78da985e0b1ff766b6cb9a
duilio.hernandez <duilio.hernandez@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-11-14 19:19:53 +0000

    fix metric

--- code/self_managment/src/main/java/com/self_managment/model/metric/MetricTimeInAuxStatus.java
+++ code/self_managment/src/main/java/com/self_managment/model/metric/MetricTimeInAuxStatus.java
@@ -1,19 +1,27 @@
 package com.self_managment.model.metric;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.self_managment.model.entity.Agent;
+import com.self_managment.service.CampaignService;
 import com.self_managment.service.SummaryService;
+import com.self_managment.service.SupervisorService;
 import com.self_managment.service.TTSService;
 
 @Component("AUX_TM")
 public class MetricTimeInAuxStatus implements MetricStrategy {
     @Autowired
     private SummaryService summaryService;
+    @Autowired
+    private CampaignService campaignService;
+    @Autowired
+    private SupervisorService supervisorService;
 
     @Autowired
     private TTSService ttService;
@@ -23,12 +31,22 @@ public class MetricTimeInAuxStatus implements MetricStrategy {
 	    Integer docket, Date dateFrom, Date dateTo) {
 	Calendar cal = Calendar.getInstance();
 	cal.setTime(dateFrom);
-	// TODO ver de cambiar la firma de getProductiveHours
-	// out hour - in hour
-	Agent agent = new Agent();
-	agent.setDocket(docket);
-	long productiveHours = ttService.getProductiveHours(agent, cal
-		.get(Calendar.MONTH) + 1, cal.get(Calendar.YEAR));
+	
+	List<Agent> agents;
+	if (campaignId != null) {
+	    agents = campaignService.findById(campaignId).getAgents();
+	} else if (supervisorId != null) {
+	    agents = supervisorService.findById(supervisorId).getAgents();
+	} else {
+	    Agent agent = new Agent();
+	    agent.setDocket(docket);
+	    agents = new ArrayList<Agent>();
+	    agents.add(agent);
+	}
+	
+	long productiveHours = 0;
+	for (Agent a : agents)
+	    productiveHours += ttService.getProductiveHours(a, dateFrom, dateTo);
 
 	return productiveHours * 60 - summaryService.getTotalLoggedTime(campaignId, supervisorId,
 			docket, dateFrom, dateTo);
