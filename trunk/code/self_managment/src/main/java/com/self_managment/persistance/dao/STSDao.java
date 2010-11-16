5e4e17a716c832bb1718c035fa5e9d251a4b5030
duilio.hernandez@gmail.com <duilio.hernandez@gmail.com@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-11-16 13:04:53 +0000

    S-01024 - Métrica schedule adherence.

--- trunk/code/self_managment/src/main/java/com/self_managment/persistance/dao/STSDao.java
+++ trunk/code/self_managment/src/main/java/com/self_managment/persistance/dao/STSDao.java
@@ -1,6 +1,7 @@
 package com.self_managment.persistance.dao;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import com.self_managment.model.entity.STS;
@@ -9,5 +10,15 @@ import com.self_managment.model.entity.STS;
 public interface STSDao extends GenericDao<STS, Serializable> {
 	
 	public List<STS> findByAgentMonthYear(Integer docket, Integer month, Integer year);
+	/**
+	 * Time in minutes
+	 * @param campaignId
+	 * @param supervisorId
+	 * @param docket
+	 * @param dateFrom
+	 * @param dateTo
+	 * @return Time in minutes
+	 */
+	public Double getScheduledAdherence(final Integer campaignId, final Integer supervisorId, final Integer docket, final Date dateFrom, final Date dateTo);
 
 }
--- trunk/code/self_managment/src/main/java/com/self_managment/service/STSService.java
+++ trunk/code/self_managment/src/main/java/com/self_managment/service/STSService.java
@@ -2,6 +2,7 @@ package com.self_managment.service;
 
 
 import java.io.Serializable;
+import java.util.Date;
 
 import com.self_managment.model.entity.Agent;
 import com.self_managment.model.entity.STS;
@@ -11,4 +12,5 @@ public interface STSService extends CRUDService<STS, Serializable> {
 
 	public double getOvertimeSalary(Agent agent, int month, int year);
 	public long getProductiveHours(Agent agent, int month, int year);
+	public Double getScheduledAdherence(Integer campaignId, Integer supervisorId, Integer docket, Date dateFrom, Date dateTo);
 }
