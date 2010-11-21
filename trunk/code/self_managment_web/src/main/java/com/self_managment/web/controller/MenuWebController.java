b128c378bb3bf6debfb2a82be7bf37df5b81f713
duilio.hernandez <duilio.hernandez@cb758c1f-a0c9-2c3e-f2b2-d67b345a71bf>
2010-11-21 14:13:16 +0000

    fix mostrar usuario y rol

--- trunk/code/self_managment_web/src/main/java/com/self_managment/web/controller/MenuWebController.java
+++ trunk/code/self_managment_web/src/main/java/com/self_managment/web/controller/MenuWebController.java
@@ -1,18 +1,19 @@
 package com.self_managment.web.controller;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.GrantedAuthority;
 import org.springframework.security.context.SecurityContextHolder;
 import org.springframework.security.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+
 @Component("menuBean")
-@Scope("request")
+@Scope("session")
 public class MenuWebController {
 	
 	private String username = "";
 	private String role = "";
-	private String loginDescription = "";
 	public static final String ACCOUNT_MANAGER = "AccountManager";
 	public static final String SUPERVISOR = "Supervisor";
 	public static final String AGENT = "Agent";
@@ -20,29 +21,33 @@ public class MenuWebController {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
+	
 	public String getUsername() {
-		Object obj = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		if (obj instanceof UserDetails) {
-			username = ((UserDetails) obj).getUsername();
-		}
+	    	if (StringUtils.isBlank(username)) {
+        		Object obj = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        		if (obj instanceof UserDetails) {
+        			username = ((UserDetails) obj).getUsername();
+        		}
+	    	}
 		return username;
 	}
 
 	public String getRole() {
-		Object obj = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		if (obj instanceof UserDetails) {
-			GrantedAuthority[] authorities = ((UserDetails) obj).getAuthorities();
-			for(int i=0; i<authorities.length; i++) {
-				if(authorities[i].equals(ACCOUNT_MANAGER)) {
-					return "Jefe de cuentas";
-				} else if(authorities[i].equals(SUPERVISOR)) {
-					return "Supervisor";
-				} else if(authorities[i].equals(AGENT)) {
-					return "Agente";
-				}
-			}
-		}
+	    	if (StringUtils.isBlank(role)) {
+        		Object obj = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        		if (obj instanceof UserDetails) {
+        			GrantedAuthority[] authorities = ((UserDetails) obj).getAuthorities();
+        			for(int i=0; i<authorities.length; i++) {
+        				if(authorities[i].equals(ACCOUNT_MANAGER)) {
+        					return "Jefe de cuentas";
+        				} else if(authorities[i].equals(SUPERVISOR)) {
+        					return "Supervisor";
+        				} else if(authorities[i].equals(AGENT)) {
+        					return "Agente";
+        				}
+        			}
+        		}
+	    	}
 		return role;
 	}
 
@@ -50,13 +55,9 @@ public class MenuWebController {
 		this.role = role;
 	}
 
-	public void setLoginDescription(String loginDescription) {
-		this.loginDescription = loginDescription;
-	}
-
 	public String getLoginDescription() {
-		if (!"".equals(username)) {
-			return "Usuario: " + username + " | Rol: " + role + " | ";
+		if (!"".equals(getUsername())) {
+			return "Usuario: " + getUsername() + " | Rol: " + getRole() + " | ";
 		} else {
 			return "";
 		}
